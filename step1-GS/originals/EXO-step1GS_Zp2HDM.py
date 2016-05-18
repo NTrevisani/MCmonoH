86860dab850fe1c5ad16df0cf8295f8cd536b060
NTrevisa <n.trevisani@campus.unimib.it>
2016-05-18 11:09:32 +0200

    adding samples, making order

--- step1-GS/originals/EXO-step1GS_Zp2HDM.py
+++ step1-GS/originals/EXO-step1GS_Zp2HDM.py
@@ -1,5 +1,13 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/ThirteenTeV/monoHiggs/pythia8_hadronizer_nomatching_HWWllnunu_cff.py --fileout file:EXO-RunIISummer15GS-02086.root --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions MCRUN2_71_V1::All --beamspot Realistic50ns13TeVCollision --step GEN,SIM --magField 38T_PostLS1 --python_filename EXO-RunIISummer15GS-02086_1_cfg.py --no_exec -n 67 --inputEventContent=LHE --filein=file:/afs/cern.ch/user/f/fatsai/public/file_1.lhe
 import FWCore.ParameterSet.Config as cms
+
 process = cms.Process('SIM')
+
+# import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
@@ -14,29 +22,37 @@ process.load('GeneratorInterface.Core.genFilterSummary_cff')
 process.load('Configuration.StandardSequences.SimIdeal_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(67)
 )
+
 # Input source
 process.source = cms.Source("LHESource",
-    fileNames = cms.untracked.vstring('/store/group/phys_exotica/monoHiggs/Zp2HDM/MZP2500_MA0500.lhe'),
-    inputCommands = cms.untracked.vstring('keep *',
+    fileNames = cms.untracked.vstring('/store/group/phys_exotica/monoHiggs/Zp2HDM/MZP600_MA0400.lhe'),
+    inputCommands = cms.untracked.vstring('keep *', 
         'drop LHEXMLStringProduct_*_*_*', 
         'drop *', 
         'keep LHERunInfoProduct_*_*_*', 
         'keep LHEEventProduct_*_*_*', 
-        'keep *_externalLHEProducer_LHEScriptOutput_*'), 
+        'keep *_externalLHEProducer_LHEScriptOutput_*'),
     dropDescendantsOfDroppedBranches = cms.untracked.bool(False)
 )
+
 process.options = cms.untracked.PSet(
+
 )
+
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
     version = cms.untracked.string('$Revision: 1.19 $'),
+    #annotation = cms.untracked.string('Configuration/GenProduction/python/ThirteenTeV/monoHiggs/pythia8_hadronizer_nomatching_HWWllnunu_cff.py nevts:67'),
     annotation = cms.untracked.string('Configuration/GenProduction/python/ThirteenTeV/Hadronizer_TuneCUETP8M1_13TeV_generic_LHE_pythia8_cff.py nevts:10'),
     name = cms.untracked.string('Applications')
 )
+
 # Output definition
+
 process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
@@ -50,11 +66,14 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
         SelectEvents = cms.vstring('generation_step')
     )
 )
+
 # Additional output definition
+
 # Other statements
 process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_71_V1::All', '')
+
 process.generator = cms.EDFilter("Pythia8HadronizerFilter",
     pythiaPylistVerbosity = cms.untracked.int32(1),
     filterEfficiency = cms.untracked.double(1.0),
@@ -62,45 +81,62 @@ process.generator = cms.EDFilter("Pythia8HadronizerFilter",
     comEnergy = cms.double(13000.0),
     maxEventsToPrint = cms.untracked.int32(1),
     PythiaParameters = cms.PSet(
-        pythia8CommonSettings = cms.vstring('Main:timesAllowErrors = 10000',
-            'Check:epTolErr = 0.01',
-            'Beams:setProductionScalesFromLHEF = off',
-            'SLHA:keepSM = on',
-            'SLHA:minMassSM = 1000.',
-            'ParticleDecays:limitTau0 = on',
-            'ParticleDecays:tau0Max = 10',
+        pythia8CommonSettings = cms.vstring('Main:timesAllowErrors = 10000', 
+            'Check:epTolErr = 0.01', 
+            'Beams:setProductionScalesFromLHEF = off', 
+            'SLHA:keepSM = on', 
+            'SLHA:minMassSM = 1000.', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tau0Max = 10', 
             'ParticleDecays:allowPhotonRadiation = on'),
-        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14',
-            'Tune:ee 7',
-            'MultipartonInteractions:pT0Ref=2.4024',
-            'MultipartonInteractions:ecmPow=0.25208',
+        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14', 
+            'Tune:ee 7', 
+            'MultipartonInteractions:pT0Ref=2.4024', 
+            'MultipartonInteractions:ecmPow=0.25208', 
             'MultipartonInteractions:expPow=1.6'),
-        processParameters = cms.vstring('25:m0 = 125.0',
-            '25:onMode = off',
-            '25:onIfMatch = 24 -24',           # turn ON H->WW
-            '24:mMin = 0.05',                  #
+        processParameters = cms.vstring('25:m0 = 125.0', 
+            '25:onMode = off', 
+	    '25:onIfMatch = 24 -24',           # turn ON H->WW
+            '24:mMin = 0.05',                  #  
             '24:onMode = off',                 # turn OFF all W decays
             '24:onIfAny = 11 13 15 12 14 16'   # turn ON W->lnu
-             ),
-        parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CUEP8M1Settings',
-                                    'processParameters'
-                                    )
+	     ),
+
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+				    'pythia8CUEP8M1Settings',
+				    'processParameters'
+				    )
     )
 )
+
+
 process.ProductionFilterSequence = cms.Sequence(process.generator)
+
 # Path and EndPath definitions
 process.generation_step = cms.Path(process.pgen)
 process.simulation_step = cms.Path(process.psim)
 process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
 # Schedule definition
 process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
 # filter all path with the production filter sequence
 for path in process.paths:
-        getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq
-from Configuration.DataProcessing.Utils import addMonitoring
+	getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
 process = addMonitoring(process)
-from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1Customs
+from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
+
+#call to customisation function customisePostLS1 imported from SLHCUpgradeSimulations.Configuration.postLS1Customs
 process = customisePostLS1(process)
+
+# End of customisation functions
